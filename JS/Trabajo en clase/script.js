aa53e96391a0ae64d59b12ee58cf9da136c82009
gladyzzz <gladys.zgarcia@prepafrayangelico.edu.mx>
2021-04-20 21:33:32 -0500

    trabajo hoy

--- JS/Trabajo en clase/script.js
+++ JS/Trabajo en clase/script.js
@@ -1,12 +1,22 @@
-var num1 = 2
-var num2 = 2 
-
-function sumar (n1,n2) {
-    if (num1 == 2 && num2 == 2){
-        alert("pez")
-
-    }else{
-    alert(n1 + n2)
+function sumar (){
+    var n1 = parseInt(prompt("Escribe un numero: "))
+    var n2 = parseInt(prompt("Escribe otro numero: "))    
+    n1 === 2 && n2 === 2 ? alert("pez") : alert(n1 + n2)
 }
+//     if(n1 == 2 && n2 == 2){
+//         alert("pez")
+//     }
+//     else{
+//         alert(n1 + n2)
+//     }
+// }
+// sumar(num1, num2)
+function mostrarFormulario(){
+    var formulario = document.getElementById('crearFormulario')
+    formulario.style.display = 'Block'
+
+   var cards = document.getElementById('cards')
+    cards.style.display = 'none'
+
+    
 }
-sumar(num1, num2)
