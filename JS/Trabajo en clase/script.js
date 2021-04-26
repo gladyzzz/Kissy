26bbb30259a69744d46669f86d157f34fe39536b
gladyzzz <gladys.zgarcia@prepafrayangelico.edu.mx>
2021-04-26 16:31:14 -0500

    video 1

--- JS/Trabajo en clase/script.js
+++ JS/Trabajo en clase/script.js
@@ -1,22 +1,41 @@
 function sumar (){
-    var n1 = parseInt(prompt("Escribe un numero: "))
-    var n2 = parseInt(prompt("Escribe otro numero: "))    
-    n1 === 2 && n2 === 2 ? alert("pez") : alert(n1 + n2)
-}
-//     if(n1 == 2 && n2 == 2){
-//         alert("pez")
-//     }
-//     else{
-//         alert(n1 + n2)
-//     }
-// }
-// sumar(num1, num2)
-function mostrarFormulario(){
+    var n1 = Number (prompt("Escribe un numero "))
+    var n2 = Number (prompt("Escribe otro numero"))    
+    if (n1 === 2 && n2 === 2 ) {
+        alert ("Pez")
+    } else {
+        alert(n1 + n2)
+    }
+        
+    }
+
+
+var verFormulario = false
+
+function mostrarFormulario() {
     var formulario = document.getElementById('crearFormulario')
-    formulario.style.display = 'Block'
+    var cards = document.getElementById('cards')
+    var crearBtn = document.getElementById('crearBtn')
+    console.log(crearBtn)
 
-   var cards = document.getElementById('cards')
-    cards.style.display = 'none'
+    if (!verFormulario) {
+        formulario.style.display = 'block'
+        cards.style.display = 'none'
+        verFormulario = true
+        crearBtn.innerText = 'Cancelar'
+    } else {
+        formulario.style.display = 'none'
+        cards.style.display = 'block'
+        verFormulario = false
+        crearBtn.innerText = 'Crear'
+    }
+    // Instrucciones para desaparecer a #cards
 
-    
+    // Cambiar el texto del boton crear por la palabra Cancelar
+    // Si oprimo una vez el boton crear, muestra el formulario y desaparecen
+    // las cards, si lo oprimo nuevamente, muestra las cards y desaparece
+    // el formulario
 }
+
+// sumar(num1, num2)
+
